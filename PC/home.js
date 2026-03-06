0df34ccddc13010bf4ebfbd3af34c6378934a9de
Udjou <hwudy340@gmail.com>
2026-03-06 20:17:40 +0500

    0.6.1

--- PC/home.js
+++ PC/home.js
@@ -1,12 +1,7 @@
-document.addEventListener("DOMContentLoaded", function() {
-    document.getElementById("button").addEventListener("click", function() {
-        document.getElementById("my-modal").classList.add("open");
-    });
+document.getElementById("button").addEventListener("click", function() {
+    document.getElementById("my-modal").classList.add("open");
 });
 
-document.addEventListener("DOMContentLoaded", function() {
-    document.getElementById("close-button").addEventListener("click", function() {
-        document.getElementById("my-modal").classList.remove("open");
-    });
-
-});
+document.getElementById("close-button").addEventListener("click", function() {
+    document.getElementById("my-modal").classList.remove("open");
+ });
---
+++ PC/version.js
@@ -0,0 +1,11 @@
+document.getElementById("button").addEventListener("click", function() {
+    window.location.href = "home.html";
+});
+
+document.getElementById("button1").addEventListener("click", function() {
+    window.location.href = "home.html";
+});
+
+document.getElementById("button2").addEventListener("click", function() {
+    window.location.href = "home.html";
+});
