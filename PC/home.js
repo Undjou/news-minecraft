211ef250ee9a7cb53b89c17e77572ddfcb233bc3
Udjou <hwudy340@gmail.com>
2026-03-06 20:14:53 +0500

    Update and rename script.js to home.js

--- PC/home.js
+++ PC/home.js
@@ -8,4 +8,5 @@ document.addEventListener("DOMContentLoaded", function() {
     document.getElementById("close-button").addEventListener("click", function() {
         document.getElementById("my-modal").classList.remove("open");
     });
+
 });
