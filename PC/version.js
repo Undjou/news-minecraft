b34c2bb9c0fb682f6ea25c3db1e7ce2985ec3b77
Udjou <hwudy340@gmail.com>
2026-03-08 20:42:10 +0500

    0.7.1

--- PC/version.js
+++ PC/version.js
@@ -1,11 +1,11 @@
 document.getElementById("button").addEventListener("click", function() {
-    window.location.href = "home.html";
+    window.location.href = "alpha.html";
 });
 
 document.getElementById("button1").addEventListener("click", function() {
-    window.location.href = "home.html";
+    window.location.href = "beta.html";
 });
 
 document.getElementById("button2").addEventListener("click", function() {
-    window.location.href = "home.html";
+    window.location.href = "release.html";
 });
